0defa94b308588c5d63a0138cffd1581c6e9235c
garretthilberling <ghilberling@knights.ucf.edu>
2022-07-02 15:39:24 -0400

    built out UserProfileController where we can get a user by user name and upload a profile image (will be encoding to base64 and stored as a string into the database)

--- TreblleApp/Controllers/UserProfileController.cs
+++ TreblleApp/Controllers/UserProfileController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using TreblleApp.Models;
 
 namespace TreblleApp.Controllers
 {
@@ -10,6 +11,32 @@ namespace TreblleApp.Controllers
     [Route("API/UserProfile")]
     public class UserProfileController : ApiController
     {
-        
+
+        // GET: api/UserProfile/userName
+        public string Get(string userName)
+        {
+            using(var context = new UserProfileDBContext())
+            {
+                var query = context.Users.FirstOrDefault(s => s.UserName == userName);
+                return query == null ? "" : query.ProfileImage;
+            }
+        }
+
+        // POST: api/UserProfile
+        // upload profile image
+        public IHttpActionResult Post([FromBody] UserProfileModel UserProfile)
+        {
+            using(var db = new UserProfileDBContext())
+            {
+                var result = db.Users.SingleOrDefault(b => b.UserName == UserProfile.UserName);
+                if(result != null)
+                {
+                    result.ProfileImage = UserProfile.ProfileImage;
+                    db.SaveChanges();
+                }
+
+                return Ok(); // will return status 200
+            }
+        }
     }
 }
