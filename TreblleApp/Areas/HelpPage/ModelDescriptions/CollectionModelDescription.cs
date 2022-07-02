dec1f37a1f82e9a364db73e45f3aaac39dfb8364
garretthilberling <ghilberling@knights.ucf.edu>
2022-07-02 16:51:46 -0400

    issue should now be resolved.

---
+++ TreblleApp/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace TreblleApp.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ TreblleApp/Controllers/UserProfileController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Treblle.Net;
+using TreblleApp.Models;
+
+namespace TreblleApp.Controllers
+{
+    [Treblle]
+    [Authorize]
+    [Route("API/UserProfile")]
+    public class UserProfileController : ApiController
+    {
+
+        // GET: api/UserProfile/userName
+        // get profile image
+        public string Get(string userName)
+        {
+            using(var context = new UserProfileDBContext())
+            {
+                var query = context.Users.FirstOrDefault(s => s.UserName == userName);
+                return query == null ? "" : query.ProfileImage;
+            }
+        }
+
+        // POST: api/UserProfile
+        // upload profile image
+        public IHttpActionResult Post([FromBody] UserProfileModel UserProfile)
+        {
+            using(var db = new UserProfileDBContext())
+            {
+                var result = db.Users.SingleOrDefault(b => b.UserName == UserProfile.UserName);
+                if(result != null)
+                {
+                    result.ProfileImage = UserProfile.ProfileImage;
+                    db.SaveChanges();
+                }
+
+                return Ok(); // will return status 200
+            }
+        }
+    }
+}
